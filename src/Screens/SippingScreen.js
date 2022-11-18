92b34aa58df8bc3730c4259076ccd79520181bdf
zzangsally <62280276+Jangchaeyun@users.noreply.github.com>
2022-11-18 22:03:29 +0900

    Signin & Register

---
+++ src/Screens/SippingScreen.js
@@ -0,0 +1,12 @@
+import { Text, View } from 'native-base'
+import React from 'react'
+
+function SippingScreen() {
+  return (
+    <View>
+      <Text>SippingScreen</Text>
+    </View>
+  )
+}
+
+export default SippingScreen
---
+++ src/data/Products.js
@@ -0,0 +1,75 @@
+const products = [
+  {
+    _id: "1",
+    name: "내셔널지오그래픽 신발 N211AFW060 뉴비 스니커즈 WHITE",
+    image:
+      "https://ifh.cc/g/yz3tZN.png",
+    description:
+      "플랫폼 형태 키높이[4cm] 스타일의 트렌디한 슈즈 데일리 및 워킹용 신발로 활용 가능 둥글한 디자인과 작은 사이즈 운영으로 여성 고객들에게 추천 다양한 기능성 적용으로 우수한 착용감 제공(아웃솔 파트 기능 우수함)",
+    price: 99000,
+    countInStock: 789,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    _id: "2",
+    name: "청키 로우 뉴욕양키스",
+    image:
+      "https://ifh.cc/g/MYPxax.png",
+    description:
+      "새로운 컬러와 포인트로 더욱 매력적인 빅볼청키 + 플레이볼 하이브리드 디자인 면 소재의 어퍼와 라이닝으로 편안하지만 견고하며, 파이론과 배합고무의 창으로 볼륨감 있는 착용감을 느낄 수 있습니다.",
+    price: 66500,
+    countInStock: 500,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    _id: "3",
+    name: "휠라 디스럽터2 DISRUPTOR 2 FS1HTB1071X 흰색 키높이 운동화",
+    image:
+      "https://ifh.cc/g/mgofXl.png",
+    description:
+      "심플한 어퍼 디자인에 플래그 로고를 적용한 깔끔한 디테일 발이 커 보이지 않는 쉐입과 볼드한 디자인이 특징인 패셔너블 슈즈 독자적인 형태의 아웃솔 디자인을 적용한 플랫폼 스니커즈",
+    price: 57870,
+    countInStock: 520,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    _id: "4",
+    name: "나이키 에어맥스 90 G",
+    image:
+      "https://ifh.cc/g/lcTXCL.png",
+    description:
+      "에어맥스 90은 깔끔한 라인과 시대를 초월한 스타일로 유명했습니다. 나이키 에어맥스 90 G는 오리지널 아이콘의 특징을 고스란히 간직하고 있으며, 통합 접지력, 전설적인 골프 코스에서 영감을 얻은 디자인 등 골프를 위한 몇 가지 기능을 추가했습니다.",
+    price: 179000,
+    countInStock: 25,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    _id: "5",
+    name: "드롭셋",
+    image:
+      "https://ifh.cc/g/hW3D5v.png",
+    description:
+      "근력 운동과 헬스 트레이닝을 새로운 차원으로 끌어올리는 아디다스 슈즈를 만나보세요. 지면과 맞닿은 느낌을 주는 낮은 미드솔이 정확한 자세를 잡을 수 있도록 도와주고 동작에 자신감을 더해줍니다. 두 가지 밀도로 구성된 EVA 쿠셔닝이 뒤꿈치에 안정감을 전달하는 동시에 앞발을 편안하게 받쳐줍니다. 갑피에 최소 50% 이상의 재생 원료가 함유되어 있으며, 플라스틱 폐기물을 없애기 위한 친환경 솔루션의 일환으로 제작된 제품입니다.",
+    price: 159000,
+    countInStock: 689,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    _id: "6",
+    name: "여성 고 워크 5_SP0WW22Y182",
+    image:
+      "https://ifh.cc/g/Z0dkFs.png",
+    description:
+      "울트라고 기술력으로 초경량 쿠셔닝 및 뛰어난 반응성을 제공합니다. 요가매트와 유사한 느낌의 인솔로 지친 발을 편안하고 쾌적하게 유지합니다. 뛰어닌 내구성의 하이퍼버스트 아웃솔은 가벼운 무게와 쫀쫀한 탄성을 자랑합니다.",
+    price: 29,
+    countInStock: 0,
+    rating: 0,
+    numReviews: 0,
+  },
+];
+export default products;
