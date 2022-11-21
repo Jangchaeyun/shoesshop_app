1b17677038e17f6d3ff7e5dcc01c9d1ed44ae4ee
zzangsally <62280276+Jangchaeyun@users.noreply.github.com>
2022-11-21 13:58:14 +0900

    Cart Screen

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 import { NativeBaseProvider } from 'native-base';
+import CartScreen from './src/Screens/CartScreen';
 import HomeScreen from './src/Screens/HomeScreen';
 import NotVerifyScreen from './src/Screens/NotVerifyScreen';
 import SingleProductScreen from './src/Screens/SingleProductScreen';
@@ -6,7 +7,7 @@ import SingleProductScreen from './src/Screens/SingleProductScreen';
 export default function App() {
   return(
     <NativeBaseProvider>
-      <SingleProductScreen />
+      <CartScreen />
     </NativeBaseProvider>
   )
 }
--- src/Screens/CartScreen.js
+++ src/Screens/CartScreen.js
@@ -1,11 +1,73 @@
-import { Text, View } from 'native-base'
+import { Box, Button, Center, HStack, ScrollView, Text } from 'native-base'
 import React from 'react'
+import Colors from "../color";
+import Buttone from '../Components/Buttone';
+// import CartEmpty from '../Components/CartEmpty';
+import CartIterms from '../Components/CartIterms';
 
 function CartScreen() {
   return (
-    <View>
-        <Text>CartScreen</Text>
-    </View>
+    <Box flex={1} safeAreaTop bg={Colors.white}>
+        {/* Header */}
+        <Center w="full" py="5">
+          <Text 
+            color={Colors.black}
+            fontSize={20} 
+            bold
+          >
+            장바구니
+          </Text>
+        </Center>
+        {/* IF CART IS EMPTY */}
+        {/* <CartEmpty /> */}
+        {/* CART ITEMS */}
+        <ScrollView showsVerticalScrollIndicator={false}>
+          <CartIterms />
+          {/* Total */}
+          <Center mt={5}>
+            <HStack
+              rounded={50}
+              justifyContent="space-between"
+              bg={Colors.white}
+              shadow={2}
+              w="90%"
+              pl={5}
+              h={45}
+              alignItems="center"
+            >
+              <Text>총액</Text>
+              <Button 
+                px={10}
+                h={45}
+                rounded={50}
+                bg={Colors.main}
+                _text={{
+                  color: Colors.white,
+                  fontWeight: "semibold",
+                }}
+                _pressed={{
+                  bg: Colors.main,
+                }}
+              >
+                165500원
+              </Button>
+            </HStack>
+          </Center>
+
+          {/* Checkout */}
+          <Center 
+            px={5}
+          >
+            <Buttone 
+              bg={Colors.black} 
+              color={Colors.white}
+              mt={10}
+            >
+              결제하기
+            </Buttone>
+          </Center>
+        </ScrollView>
+    </Box>
   )
 }
 
