f5f6cb9194adb0a1fec4f13fe2d47358234f14db
zzangsally <62280276+Jangchaeyun@users.noreply.github.com>
2022-11-23 22:49:41 +0900

    BottomNav

--- App.js
+++ App.js
@@ -1,12 +1,42 @@
-import { NativeBaseProvider } from 'native-base';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NativeBaseProvider, StatusBar } from 'native-base';import BottomNav from './src/Navigations/BottomNav';
+import LoginScreen from './src/Screens/LoginScreen';
 import OrderScreen from './src/Screens/OrderScreen';
-import PaymentScreen from './src/Screens/PaymentScreen';
-import PlaceOrderScreen from './src/Screens/PlaceOrderScreen';
+import RegisterScreen from './src/Screens/RegisterScreen';
+;
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-  return(
+  return (
     <NativeBaseProvider>
-      <OrderScreen />
+      <NavigationContainer>
+        <StatusBar hidden={true} />
+        <Stack.Navigator
+          initialRouteName="Bottom"
+          screenOptions={{
+            headerShown: false,
+          }}
+        >
+          <Stack.Screen
+            name="Login"
+            component={LoginScreen} 
+          />
+          <Stack.Screen
+            name="Register"
+            component={RegisterScreen} 
+          />
+          <Stack.Screen
+            name="order"
+            component={OrderScreen} 
+          />
+          <Stack.Screen
+            name="Bottom"
+            component={BottomNav} 
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
     </NativeBaseProvider>
   )
 }
---
+++ src/Navigations/BottomNav.js
@@ -0,0 +1,100 @@
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Center, Pressable } from 'native-base';
+import { Entypo, AntDesign, FontAwesome, FontAwesome5, MaterialCommunityIcons } from '@expo/vector-icons';
+import React from 'react';
+import { Text, StyleSheet } from 'react-native';
+import Colors from '../color';
+import HomeScreen from '../Screens/HomeScreen'
+import ProfileScreen from '../Screens/ProfileScreen';
+import CartScreen from '../Screens/CartScreen';
+
+const Tab = createBottomTabNavigator();
+const CustomTab = ({ children, onPress }) => (
+    <Pressable
+        onPress={onPress}
+        h={70}
+        w={70}
+        _pressed={{
+            bg:Colors.black
+        }}
+        rounded="full"
+        bg={Colors.main}
+        top={-30}
+        shadow={2}
+    >
+        {children}
+    </Pressable>
+);
+const BottomNav = () => {
+  return (
+    <Tab.Navigator
+        backBehavior="main"
+        initialRouteName="Main"
+        screenOptions={{
+            tabBarShowLabel: false,
+            tabBarStyle:{ ...styles.tab },
+            headerShown:false,
+            tabBarHideOnKeyboard:true
+        }}
+    >
+        <Tab.Screen
+            name="main"
+            component={HomeScreen}
+            options={{
+                tabBarIcon:({ focused }) => (
+                    <Center>
+                        {focused ? (
+                            <Entypo name="home" size={24} color={Colors.main} />
+                        ) : (
+                            <AntDesign name="home" size={24} color={Colors.lightBlack} />
+                        )}
+                    </Center>
+                ),
+            }}
+        />
+        {/* Cart */}
+        <Tab.Screen
+            name="Cart"
+            component={CartScreen}
+            options={{
+                tabBarButton: (props) => <CustomTab {...props} />,
+                tabBarIcon: ({ focused }) => (
+                    <Center>
+                        {focused ? (
+                            <FontAwesome5 name="shopping-basket" size={24} color={Colors.white} />
+                        ) : (
+                            <MaterialCommunityIcons name="shopping-outline" size={24} color={Colors.white} />
+                        )}
+                    </Center>
+                ),
+            }}
+        />
+        {/* profile */}
+        <Tab.Screen
+            name="Profile"
+            component={ProfileScreen}
+            options={{
+                tabBarIcon:({ focused }) => (
+                    <Center>
+                        {focused ? (
+                            <FontAwesome name="user" size={24} color={Colors.main} />
+                        ) : (
+                            <AntDesign name="user" size={24} color={Colors.lightBlack} />
+                        )}
+                    </Center>
+                ),
+            }}
+        />
+    </Tab.Navigator>
+  );
+}
+
+const styles = StyleSheet.create({
+    tab: {
+        elevation:0,
+        backgroundColor: Colors.white,
+        height: 60,
+    }
+})
+
+export default BottomNav;
